bfc6ecbc9537b1a85a301856b45a0a3225a86963
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2022-12-04 11:48:20 +0300

    Create 3_How Many Numbers Are Smaller Than the Current Number.py

---
+++ leetcode_problems/3_How Many Numbers Are Smaller Than the Current Number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+
+
+    def smallerNumbersThanCurrent(self, nums):
+        l=len(nums)
+        ans=[]
+        count=0
+        for i in nums:
+            for j in range(l):
+               if (nums[j]-i)<0:
+                   count+=1
+            ans.append(count)
+            count=0
+        return ans
+
