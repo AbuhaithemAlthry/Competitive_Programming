f6f7c69d682f37143a5adab6477333e0c426063b
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2023-01-18 21:18:06 +0300

    Create 25_Reverse Substrings Between Each Pair of Parentheses.py

---
+++ leetcode_problems/25_Reverse Substrings Between Each Pair of Parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        res = ['']
+        for c in s:
+            if c == '(':
+                res.append('')
+            elif c == ')':
+                res[len(res) - 2] += res.pop()[::-1]
+            else:
+                res[-1] += c
+            print(res)
+        return "".join(res)
+    
