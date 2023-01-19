8e672fee5ee31f5ebd9c67266f1c3dd6b94fff46
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2023-01-19 09:07:14 +0300

    Update 29_Next Greater Element I.py

--- leetcode_problems/29_Next Greater Element I.py
+++ leetcode_problems/29_Next Greater Element I.py
@@ -1,15 +1,20 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        n=len(nums1)
-        m=len(nums2)
-        ret=[-1]*n
-        for i in range(n):
-            ind=nums2.index(nums1[i])
-            for j in range(ind,m):
-                if nums2[j]>nums1[i]:
-                    ret[i]=nums2[j]
-                    break
-        return ret
-
-
-
+        stack = []
+        tempDict = {}
+        for i in nums2:
+            while stack and stack[-1] < i:
+                poppedNum = stack.pop()
+                tempDict[poppedNum] = i
+                print(tempDict)
+            stack.append(i)
+            print(stack,tempDict)
+            
+        for i in range(len(nums1)):
+            if nums1[i] in tempDict:
+                nums1[i] = tempDict[nums1[i]]
+                
+            else:
+                nums1[i] = -1
+        
+        return nums1
